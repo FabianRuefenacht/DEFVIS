77163fca1988989e2840e61e6fd7adde5018e829
Fabian Rüfenacht <f.ruefenacht@outlook.com>
2024-04-08 22:52:08 +0200

    select in Map,  show in Model

--- frontend/src/app/components/Detail.tsx
+++ frontend/src/app/components/Detail.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-function Detail({ baseSessionPoints, nextSessionPoints }: { baseSessionPoints: any, nextSessionPoints: any }) {
+function Detail({
+  baseSessionPoints,
+  nextSessionPoints,
+  view3DPoint,
+}: {
+  baseSessionPoints: any;
+  nextSessionPoints: any;
+  view3DPoint: string | null;
+}) {
   const [viewwModel, setViewwModel] = useState("Text");
-  const [basePts, setBasePts] = useState([]);
-  const [nextPts, setNextPts] = useState([]);
+  const [basePts, setBasePts] = useState<Point[]>([]);
+  const [nextPts, setNextPts] = useState<Point[]>([]);
   useEffect(() => {
     try {
       if (baseSessionPoints) {
@@ -33,60 +41,120 @@ function Detail({ baseSessionPoints, nextSessionPoints }: { baseSessionPoints: a
     H: number;
   }
 
-  function findCorrespondingNextPoint(basePoint: Point, nextPoints: Point[]): Point | undefined {
-    return nextPoints.find(point => point.name === basePoint.name);
+  const [view3DPointInMap, setView3DPointInMap] = useState<string | null>(null);
+
+  const [view3DPointInMapEasting, setView3DPointInMapEasting] = useState<number | null>(null);
+  const [view3DPointNextEasting, setView3DPointNextEasting] = useState<number | null>(null);
+
+  const [view3DPointInMapNorthing, setView3DPointInMapNorthing] = useState<number | null>(null);
+  const [view3DPointNextNorthing, setView3DPointNextNorthing] = useState<number | null>(null);
+
+  const [view3DPointInMapHeight, setView3DPointInMapHeight] = useState<number | null>(null);
+  const [view3DPointNextHeight, setView3DPointNextHeight] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (view3DPoint && basePts && nextPts) {
+      const view3DPointInMapEasting = basePts.find((point) => point.name === view3DPoint)?.E;
+      const view3DPointNextEasting = nextPts.find((point) => point.name === view3DPoint)?.E;
+      const view3DPointInMapNorthing = basePts.find((point) => point.name === view3DPoint)?.N;
+      const view3DPointNextNorthing = nextPts.find((point) => point.name === view3DPoint)?.N;
+      const view3DPointInMapHeight = basePts.find((point) => point.name === view3DPoint)?.H;
+      const view3DPointNextHeight = nextPts.find((point) => point.name === view3DPoint)?.H;
+  
+      setView3DPointInMap(view3DPoint);
+      setViewwModel("3D");
+      {view3DPointInMapEasting ? setView3DPointInMapEasting(view3DPointInMapEasting) : "";}
+      {view3DPointNextEasting ? setView3DPointNextEasting(view3DPointNextEasting) : "";}
+      {view3DPointInMapNorthing ? setView3DPointInMapNorthing(view3DPointInMapNorthing) : "";}
+      {view3DPointNextNorthing ? setView3DPointNextNorthing(view3DPointNextNorthing) : "";}
+      {view3DPointInMapHeight ? setView3DPointInMapHeight(view3DPointInMapHeight) : "";}
+      {view3DPointNextHeight ? setView3DPointNextHeight(view3DPointNextHeight) : "";}
+    }
+  }, [view3DPoint, basePts, nextPts]);
+  
+
+  function findCorrespondingNextPoint(
+    basePoint: Point,
+    nextPoints: Point[]
+  ): Point | undefined {
+    return nextPoints.find((point) => point.name === basePoint.name);
   }
-
-
 
   return (
     <main>
-      <button onClick={() => setViewwModel("Text")} className='pl-5'>Text</button>
-      <button onClick={() => setViewwModel("ZRA")} className='pl-5'>ZRA</button>
-      <button onClick={() => setViewwModel("3D")} className='pl-5'>3D</button>
-      {viewwModel === "Text" && basePts[0] && nextPts[0] &&
+      <button onClick={() => setViewwModel("Text")} className="pl-5">
+        Text
+      </button>
+      <button onClick={() => setViewwModel("ZRA")} className="pl-5">
+        ZRA
+      </button>
+      <button onClick={() => setViewwModel("3D")} className="pl-5">
+        3D
+      </button>
+      {viewwModel === "Text" && basePts[0] && nextPts[0] && (
         <div className="overflow-y-auto h-full p-4 pb-10">
           <table>
             <thead>
-              <tr className='p-0'>
-                <th className='p-0 pr-5 text-left'>
-                  Punkt
-                </th>
-                <th className='p-0 pr-5 text-left'>
-                  ΔE [mm]
-                </th>
-                <th className='p-0 pr-5 text-left'>
-                  ΔN [mm]
-                </th>
-                <th className='p-0 text-left'>
-                  ΔH [mm]
-                </th>
+              <tr className="p-0">
+                <th className="p-0 pr-5 text-left">Punkt</th>
+                <th className="p-0 pr-5 text-left">ΔE [mm]</th>
+                <th className="p-0 pr-5 text-left">ΔN [mm]</th>
+                <th className="p-0 text-left">ΔH [mm]</th>
               </tr>
             </thead>
             <tbody>
-              {basePts.map((point: { "name": string, "E": number, "N": number, "H": number }) => {
-                const correspondingNextPoint = findCorrespondingNextPoint(point, nextPts)
-                const deltaOst = correspondingNextPoint ? correspondingNextPoint.E - point.E : point.E;
-                const deltaNord = correspondingNextPoint ? correspondingNextPoint.N - point.N : point.N;
-                const deltaH = correspondingNextPoint ? correspondingNextPoint.H - point.H : point.H;
+              {basePts.map(
+                (point: { name: string; E: number; N: number; H: number }) => {
+                  const correspondingNextPoint = findCorrespondingNextPoint(
+                    point,
+                    nextPts
+                  );
+                  const deltaOst = correspondingNextPoint
+                    ? correspondingNextPoint.E - point.E
+                    : point.E;
+                  const deltaNord = correspondingNextPoint
+                    ? correspondingNextPoint.N - point.N
+                    : point.N;
+                  const deltaH = correspondingNextPoint
+                    ? correspondingNextPoint.H - point.H
+                    : point.H;
 
-                return (
-                  <tr key={point.name}>
-                    <td className='pr-5 text-left'>{point.name}</td>
-                    <td className='pr-5 text-right'>{(deltaOst * 1000).toFixed(1)}</td>
-                    <td className='pr-5 text-right'>{(deltaNord * 1000).toFixed(1)}</td>
-                    <td className='text-right'>{(deltaH * 1000).toFixed(1)}</td>
-                  </tr>
-                );
-              })}
+                  return (
+                    <tr key={point.name}>
+                      <td className="pr-5 text-left">{point.name}</td>
+                      <td className="pr-5 text-right">
+                        {(deltaOst * 1000).toFixed(1)}
+                      </td>
+                      <td className="pr-5 text-right">
+                        {(deltaNord * 1000).toFixed(1)}
+                      </td>
+                      <td className="text-right">
+                        {(deltaH * 1000).toFixed(1)}
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
             </tbody>
           </table>
         </div>
-      }
+      )}
 
+      {viewwModel === "3D" && view3DPointInMap && view3DPointNextEasting && view3DPointNextNorthing && (
+        <div className=" pl-5">
+          <p>Punktnummer: {view3DPoint}</p>
+          <p>Ost: {view3DPointInMapEasting}</p>
+          <p>Δ Ost: {view3DPointNextEasting && view3DPointInMapEasting ? `${((view3DPointNextEasting - view3DPointInMapEasting) * 1000).toFixed(1)} mm` : ""}</p>
+          <p>Nord: {view3DPointInMapNorthing}</p>
+          <p>Δ Nord: {view3DPointNextNorthing && view3DPointInMapNorthing ? `${((view3DPointNextNorthing - view3DPointInMapNorthing) * 1000).toFixed(1)} mm` : ""}</p>
+          <p>Höhe: {view3DPointInMapHeight}</p>
+          <p>Δ Höhe: {view3DPointNextHeight && view3DPointInMapHeight ? `${((view3DPointNextHeight - view3DPointInMapHeight) * 1000).toFixed(1)} mm` : ""}</p>
 
+          // ToDo insert 3D-Model
+        </div>
+      )}
     </main>
-  )
+  );
 }
 
-export default Detail
+export default Detail;
--- frontend/src/app/components/OlMap.tsx
+++ frontend/src/app/components/OlMap.tsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useRef } from 'react';
-import 'ol/ol.css';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import { fromLonLat } from 'ol/proj';
-import Feature from 'ol/Feature';
-import Point from 'ol/geom/Point';
-import { Vector as VectorLayer } from 'ol/layer';
-import { Vector as VectorSource } from 'ol/source';
-import { Style, Circle, Fill, Stroke, Text } from 'ol/style';
+import React, { useEffect, useRef, useState } from "react";
+import "ol/ol.css";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import { Vector as VectorLayer } from "ol/layer";
+import { Vector as VectorSource } from "ol/source";
+import { Style, Circle, Fill, Stroke, Text } from "ol/style";
+import Overlay from "ol/Overlay";
 
 import {
   DragRotateAndZoom,
   defaults as defaultInteractions,
-} from 'ol/interaction.js';
+} from "ol/interaction.js";
 
-import Projection from 'ol/proj/Projection.js';
-import TileWMS from 'ol/source/TileWMS.js';
-import { FullScreen, Rotate, defaults as defaultControls } from 'ol/control.js';
+import Projection from "ol/proj/Projection.js";
+import TileWMS from "ol/source/TileWMS.js";
+import { FullScreen, Rotate, defaults as defaultControls } from "ol/control.js";
 
 interface IfPoint {
   pointId: number;
@@ -27,72 +27,85 @@ interface IfPoint {
   N: number;
   H: number;
 }
-// ...
 
-const OlMap = ({ bbox, pts }: { bbox: [number, number], pts: IfPoint[] }) => {
+const OlMap = ({ bbox, pts, handle3DClick }: { bbox: [number, number]; pts: IfPoint[]; handle3DClick: (pointId: string | null) => void }) => {
   const mapRef = useRef<HTMLDivElement>(null);
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const [overlayContent, setOverlayContent] = useState("");
+  const [selectedPointName, setSelectedPointName] = useState<string>("null");
 
   useEffect(() => {
-    if (!mapRef.current) return;
+    if (!mapRef.current || !overlayRef.current) return;
 
     const extent = [2420000, 130000, 2900000, 1350000];
 
     const swisstopolayer = new TileLayer({
       extent: extent,
       source: new TileWMS({
-        url: 'https://wms.geo.admin.ch/',
-        crossOrigin: 'anonymous',
-        attributions: '© <a href="http://www.geo.admin.ch/internet/geoportal/' + 'en/home.html">geo.admin.ch</a>',
-        projection: 'EPSG:2056',
+        url: "https://wms.geo.admin.ch/",
+        crossOrigin: "anonymous",
+        attributions:
+          '© <a href="http://www.geo.admin.ch/internet/geoportal/' +
+          'en/home.html">geo.admin.ch</a>',
+        projection: "EPSG:2056",
         params: {
-          'LAYERS': 'ch.swisstopo.landeskarte-farbe-10',
-          'FORMAT': 'image/jpeg'
+          LAYERS: "ch.swisstopo.landeskarte-farbe-10",
+          FORMAT: "image/jpeg",
         },
-        serverType: 'mapserver'
-      })
+        serverType: "mapserver",
+      }),
     });
 
     const map = new Map({
-      controls: defaultControls().extend([new FullScreen(), new Rotate({ duration: 1000 })]),
+      controls: defaultControls().extend([
+        new FullScreen(),
+        new Rotate({ duration: 1000 }),
+      ]),
       interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
       target: mapRef.current,
-      layers: [
-        swisstopolayer
-      ],
+      layers: [swisstopolayer],
       view: new View({
         center: bbox,
         zoom: 15,
         projection: new Projection({
-          code: 'EPSG:2056',
-          units: 'm'
-        })
-      })
+          code: "EPSG:2056",
+          units: "m",
+        }),
+      }),
     });
 
     // Convert points to features with individual circle style
     const features = pts.map((point) => {
-      const geometry = new Point(([point.E, point.N]));
-      return new Feature(geometry);
+      const geometry = new Point([point.E, point.N]);
+      return new Feature({
+        geometry: geometry,
+        name: point.name,
+        easting: point.E,
+        northing: point.N,
+        height: point.H,
+        pointId: point.pointId, // Store the pointId in the feature
+      });
     });
 
     features.forEach((feature, index) => {
-      feature.setStyle(new Style({
-        image: new Circle({
-          radius: 5, // Radius of the circle representing the point
-          fill: new Fill({ color: 'red' }), // Fill color of the circle
-          stroke: new Stroke({ color: 'black', width: 1 }) // Stroke color and width of the circle
-        }),
-        text: new Text({
-          text: pts[index].name.toString(), // Set the text to the point name
-          offsetX: 15, // Offset the text horizontally from the point
-          offsetY: -15, // Offset the text vertically from the point
-          fill: new Fill({ color: 'black' }), // Fill color of the text
-          font: 'bold 12px Arial', // Font style of the text
-          backgroundFill: new Fill({ color: 'white' }), // Background fill color of the text
+      feature.setStyle(
+        new Style({
+          image: new Circle({
+            radius: 5, // Radius of the circle representing the point
+            fill: new Fill({ color: "red" }), // Fill color of the circle
+            stroke: new Stroke({ color: "black", width: 1 }), // Stroke color and width of the circle
+          }),
+          text: new Text({
+            text: pts[index].name.toString(), // Set the text to the point name
+            offsetX: 15, // Offset the text horizontally from the point
+            offsetY: -15, // Offset the text vertically from the point
+            fill: new Fill({ color: "black" }), // Fill color of the text
+            font: "bold 12px Arial", // Font style of the text
+            backgroundFill: new Fill({ color: "white" }), // Background fill color of the text
+          }),
         })
-      }));
+      );
     });
-
 
     // Create vector source and layer
     const vectorSource = new VectorSource({
@@ -106,14 +119,63 @@ const OlMap = ({ bbox, pts }: { bbox: [number, number], pts: IfPoint[] }) => {
 
     map.addLayer(vectorLayer);
 
+    // Create overlay
+    const overlay = new Overlay({
+      element: overlayRef.current,
+      positioning: "bottom-center",
+      offset: [0, -10], // Offset from the bottom center of the map
+    });
+
+    map.addOverlay(overlay);
+
+    map.on("click", function (e) {
+      overlay.setPosition(undefined);
+      map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
+        let clickedName = feature.get("name");
+        let clickedEasting = feature.get("easting");
+        let clickedNorthing = feature.get("northing");
+        let clickedHeight = feature.get("height");
+        let clickedPointId = feature.get("pointId"); // Get the pointId from the feature
+
+        setOverlayContent(
+          `${clickedName}<br>Easting: ${clickedEasting}<br>Northing: ${clickedNorthing}<br>Height: ${clickedHeight} <br><br>`
+        );
+
+        setSelectedPointName(clickedName.toString()); // Set the selected pointId
+        overlay.setPosition(e.coordinate); // Set overlay position to clicked coordinate
+      });
+    });
+
     return () => {
       map.dispose(); // Dispose the map when component unmounts
     };
   }, [bbox, pts]); // Include bbox and points in the dependency array
-  
 
 
-  return <div ref={mapRef} className="map" style={{ width: '100%', height: '100%', background: '#FFFFFF' }} />;
+
+  return (
+    <div>
+      <div
+        ref={mapRef}
+        className="map"
+        style={{ width: "100%", height: "100%", background: "#FFFFFF" }}
+      />
+      <div
+        ref={overlayRef}
+        className=" bg-slate-200 text-slate-900 rounded-xl text-sm p-3"
+      >
+        <div dangerouslySetInnerHTML={{ __html: overlayContent }}></div>
+        {selectedPointName !== null && (
+          <button
+            className=" bg-cyan-700 text-cyan-50 px-4 py-2 rounded-2xl"
+            onClick={() => handle3DClick(selectedPointName)}
+          >
+            3D-Modell laden
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default OlMap;
