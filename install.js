24fe51163cd98eaa3be547f67285a28c11b07510
Fabian Rüfenacht <f.ruefenacht@outlook.com>
2024-05-08 10:07:11 +0200

    update insta..js

--- install.js
+++ install.js
@@ -3,41 +3,44 @@ const path = require('path');
 
 const frontend = './frontend';
 const backend = './backend';
-const requirements = './backend/requirements.txt';
+const requirements = 'requirements.txt'; // Relativer Pfad zur requirements.txt-Datei im Backend-Verzeichnis
 
 const projname = "test";
-
-// Plattformunabhängige Pfadkonstruktion
-const backendPath = path.join(__dirname, backend);
-const frontendPath = path.join(__dirname, frontend);
-const requirementsPath = path.join(__dirname, requirements);
 
 // Funktion zum Ausführen von Befehlen und Anzeigen von Ausgaben
 function executeCommand(command, cwd, description) {
   console.log(`Starte Befehl: ${description}`);
   console.log(`Befehl: ${command}`);
   
-  const childProcess = exec(command, { cwd });
+  return new Promise((resolve, reject) => {
+    const childProcess = exec(command, { cwd });
+    
+    childProcess.stdout.on('data', (data) => {
+      console.log(`[stdout] ${data}`);
+    });
   
-  childProcess.stdout.on('data', (data) => {
-    console.log(`[stdout] ${data}`);
-  });
-
-  childProcess.stderr.on('data', (data) => {
-    console.error(`[stderr] ${data}`);
-  });
-
-  childProcess.on('close', (code) => {
-    console.log(`Befehl ${description} beendet mit Exit-Code ${code}`);
+    childProcess.stderr.on('data', (data) => {
+      console.error(`[stderr] ${data}`);
+    });
+  
+    childProcess.on('close', (code) => {
+      console.log(`Befehl ${description} beendet mit Exit-Code ${code}`);
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(`Fehler beim Ausführen des Befehls ${description}`);
+      }
+    });
   });
 }
 
-// Backend-Befehle ausführen
-executeCommand(`conda create ${projname}`, backendPath, 'Erstelle Conda-Umgebung');
-executeCommand(`conda activate ${projname}`, backendPath, 'Aktiviere Conda-Umgebung');
-executeCommand(`pip install -r ${requirementsPath}`, backendPath, 'Installiere Python-Module');
-executeCommand(`python ${path.join(backendPath, 'main.py')}`, backendPath, 'Starte Backend');
-
-// Frontend-Befehle ausführen
-executeCommand(`npm install`, frontendPath, 'Installiere npm-Pakete für Frontend');
-executeCommand(`npm run dev`, frontendPath, 'Starte Frontend-Entwicklungsmodus');
+// Backend-Befehle nacheinander ausführen
+executeCommand(`conda activate ${projname}`, backend, 'Aktiviere Conda-Umgebung')
+  .then(() => executeCommand(`pip install -r ${path.join(__dirname, backend, requirements)}`, backend, 'Installiere Python-Module'))
+  .then(() => {
+    // Frontend-Befehle ausführen
+    return executeCommand(`npm install`, frontend, 'Installiere npm-Pakete für Frontend');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
